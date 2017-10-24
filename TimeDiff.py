c0d3e72b52e82459d3ea73f43e495d2ec8fe9910
Rob A.J. Eyles <raje1@xroa-dt-20.star.le.ac.uk>
2017-10-24 11:41:39 +0100

    Output now in seconds and days

--- TimeDiff.py
+++ TimeDiff.py
@@ -1,17 +1,21 @@
 #Needed to work
 import datetime
 from datetime import datetime
+from datetime import timedelta
 
 #Time differences are calculated from here
-TriggerTime = raw_input("Please enter the initial trigger date in the format 'yyyy/mm/dd hh:mm:ss': ")
+TriggerTime = raw_input("Please enter the initial trigger date in the format 'yyyy/mm/dd hh:mm:ss.ss': ")
 
-ObsTime = raw_input("Please enter the observation time in the format 'yyyy/mm/dd hh:mm:ss': ")
+ObsTime = raw_input("Please enter the observation time in the format 'yyyy/mm/dd hh:mm:ss.ss': ")
 
-FMT = '%Y/%m/%d %H:%M:%S'
+FMT = '%Y/%m/%d %H:%M:%S.%f'
 
 TimeDiff = datetime.strptime(ObsTime, FMT) - datetime.strptime(TriggerTime, FMT)
 
-print(TriggerTime)
-print(ObsTime)
-print(TimeDiff)
-print("Brian")
+print("Time difference in seconds:")
+print(TimeDiff.total_seconds())
+
+TimeDiffDays=float(TimeDiff.total_seconds()/86400)
+
+print("Time difference in days:")
+print(TimeDiffDays)
